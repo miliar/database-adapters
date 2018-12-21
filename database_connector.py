21bf0f0603d889c057aba817e64a4e2db5be9e2c
Rashid <miliar1732@gmail.com>
2018-12-21 17:13:07 +0100

    added function to get the result from a query as iterator

--- database_connector.py
+++ database_connector.py
@@ -13,15 +13,22 @@ class DatabaseConnector():
 
     def __exit__(self, exception_type, exception_value, traceback):
         if traceback:
-            print(f'Error : {exception_type} {exception_value}') # implement logging
+            print(f'Error : {exception_type} {exception_value}')  # todo : log
         self.cursor.close()
         self.connection.close()
 
-    def print_query(self, query):
+    def get_result_iter(self, query, chunksize=10):
         self.cursor.execute(query)
-        print(self.cursor.fetchall())
+        while True:
+            rows = self.cursor.fetchmany(chunksize)
+            if rows:
+                yield rows
+            else:
+                break
 
 
 with DatabaseConnector(db_config) as dbc:
-    query = ('select * from watchlists limit 2')
-    dbc.print_query(query)
+    query = 'select * from watchlists limit 20'
+    result = dbc.get_result_iter(query, 1)
+    for rows in result:
+        print(rows)
