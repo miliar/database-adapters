65b925e5028be418e67d8fba169714a586ff00b2
Rashid <miliar1732@gmail.com>
2018-12-26 14:15:14 +0100

    added class for saving query results

--- adapter_bigquery.py
+++ adapter_bigquery.py
@@ -1,10 +1,26 @@
 from google.cloud import bigquery
+import logging
+import traceback as tb
 
 
 class AdapterBigquery():
     def __init__(self, service_acc):
         self.__client = bigquery.Client.from_service_account_json(service_acc)
+        self.__logger = logging.getLogger(__name__)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exception):
+        self.__handle_exception(*exception)
+
+    def __handle_exception(self, exc_type, exc_value, traceback):
+        if traceback:
+            exception = tb.format_exception(exc_type, exc_value, traceback)
+            self.__logger.error(exception)
 
     def get_result_iter(self, query):
         query_job = self.__client.query(query)
-        return query_job.result()
+        result = query_job.result()
+        for row in result:
+            yield row.values()
---
+++ query_result_saver_test.py
@@ -0,0 +1,38 @@
+from query_result_saver import QueryResultSaver
+from mysql_testconfig import DB_CONFIG, SAVE_PATH, QUERY as query_mysql
+from adapter_mysql import AdapterMysql
+from bigquery_testconfig import SERVICE_ACC, QUERY as query_bigquery
+from adapter_bigquery import AdapterBigquery
+import unittest
+import csv
+
+
+class TestQueryResultSaver(unittest.TestCase):
+    def setUp(self):
+        self.save_path = SAVE_PATH + '/test.csv'
+        with open(SAVE_PATH + '/test_bigquery.csv', newline='') as bq_csv:
+            self.result_bigquery = list(csv.reader(
+                bq_csv, delimiter=' ', quotechar='|'))
+        with open(SAVE_PATH + '/test_mysql.csv', newline='') as mysql_csv:
+            self.result_mysql = list(csv.reader(
+                mysql_csv, delimiter=' ', quotechar='|'))
+
+    def test_result_to_csv_with_bigquery(self):
+        with AdapterBigquery(SERVICE_ACC) as adapter:
+            QueryResultSaver(query_bigquery, adapter).result_to_csv(
+                self.save_path)
+        with open(self.save_path, newline='') as result_csv:
+            result = csv.reader(result_csv, delimiter=' ', quotechar='|')
+            self.assertEqual(list(result), self.result_bigquery)
+
+    def test_result_to_csv_with_mysql(self):
+        with AdapterMysql(DB_CONFIG) as adapter:
+            QueryResultSaver(query_mysql, adapter).result_to_csv(
+                self.save_path)
+        with open(self.save_path, newline='') as result_csv:
+            result = csv.reader(result_csv, delimiter=' ', quotechar='|')
+            self.assertEqual(list(result), self.result_mysql)
+
+
+if __name__ == '__main__':
+    unittest.main()
